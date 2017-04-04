022c072fbbf19d9ef49d52a4a386e0ecf894291b
Estelle Martinez <rikku.shimizu@gmail.com>
2017-04-04 18:23:11 +0200

    Work details page: screens, description and links

--- src/Work/WorkDetails/index.js
+++ src/Work/WorkDetails/index.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import GoLinkExternal from 'react-icons/lib/go/link-external';
 import './WorkDetails.css';
+
+const getScreens = (project, type) => {
+  if (type && project.type) {
+    return project.type;
+  }
+
+  if (project.mobile) {
+    return project.mobile;
+  }
+
+  if (project.desktop) {
+    return project.desktop;
+  }
+
+  return [];
+}
+
+const screen = (screen, i) => {
+  return <div className="screen" key={`screen${i}`} style={{backgroundImage: `url("/images/screens/${screen}")`}} />
+};
 
 const detailsContent = project => (
   <div className="work-details-content">
-    <h2>{project.title}</h2>
-    <p>{project.description}</p>
-    <div className="screen" style={{backgroundImage: `url("/images/screens/${project.screens[0]}")`}} />
+    <h2 style={{color: project.color}}>{project.title}</h2>
+    <div className="screens" style={{backgroundColor: project.color}}>
+      <p>
+        {project.description}
+        {project.links.map((link, i) => (
+          <div>
+            <GoLinkExternal />
+            <a key={`link${i}`} href={link.url} target="blank">
+              {link.title}
+            </a>
+          </div>
+        ))}
+      </p>
+      {getScreens(project).map(screen)}
+    </div>
   </div>
 );
 
